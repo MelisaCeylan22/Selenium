c083f351db68705e0351103957f39a94f6e2f9c6
MelisaCeylan22 <melisacy22@outlook.com>
2026-01-26 13:13:37 +0300

    Finding

--- src/seleniumtest/_06_FindingByLinkText.java
+++ src/seleniumtest/_06_FindingByLinkText.java
@@ -1,11 +1,22 @@
 package seleniumtest;
 
+import Utilities.MyFunctions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class _06_FindingByLinkText {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://form.jotform.com/260212204635040");
+        driver.get("https://www.hepsiburada.com/");
+
+        WebElement kampanyalar = driver.findElement(By.linkText("Kampanyalar"));
+        System.out.println("kampanyalar.getText() = " + kampanyalar.getText());
+
+        MyFunctions.Bekleme(4);
+        driver.quit();
+
+
     }
 }
